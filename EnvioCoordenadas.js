83c74d2bd9766cb084eb0079d9e5be5c23f056b2
fibercom <68404829+fibercom@users.noreply.github.com>
2020-07-16 21:05:36 -0500

    Update EnvioCoordenadas.js

--- EnvioCoordenadas.js
+++ EnvioCoordenadas.js
@@ -8,7 +8,7 @@ function MensajeLog(text) {
 function EnviaCoordenasQualitas() {
     var request = new XMLHttpRequest();
 
-    request.open("POST", "http://qa.qualitas.com.mx:7005/PMQ-API-MOVIL/rest/grua/coordenadaExterna", true);
+    request.open("POST", "https://qa.qualitas.com.mx:7005/PMQ-API-MOVIL/rest/grua/coordenadaExterna", true);
     request.setRequestHeader("Authorization", "Basic cmVzdGdhcHA6U1JZejY6U3BVXA==");
     //request.setRequestHeader("Host", "qa.qualitas.com.mx");
     request.setRequestHeader("Content-Type", "application/json");
