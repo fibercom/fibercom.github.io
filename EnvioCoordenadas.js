5b7558faec44ada8b838f19022ac82dcdb92271e
fibercom <68404829+fibercom@users.noreply.github.com>
2020-07-16 20:47:27 -0500

    Update EnvioCoordenadas.js

--- EnvioCoordenadas.js
+++ EnvioCoordenadas.js
@@ -1,10 +1,16 @@
 var Contador = 3;
 
+function MensajeLog(text) {
+    $("#Bitacora").prepend(text + "<br/>");
+}
+
+
 function EnviaCoordenasQualitas() {
     var request = new XMLHttpRequest();
-    request.open("POST", "https://qa.qualitas.com.mx:7005/PMQ-API-MOVIL/rest/grua/coordenadaExterna", false);
+
+    request.open("POST", "http://qa.qualitas.com.mx:7005/PMQ-API-MOVIL/rest/grua/coordenadaExterna", true);
     request.setRequestHeader("Authorization", "Basic cmVzdGdhcHA6U1JZejY6U3BVXA==");
-    request.setRequestHeader("Host", "qa.qualitas.com.mx");
+    //request.setRequestHeader("Host", "qa.qualitas.com.mx");
     request.setRequestHeader("Content-Type", "application/json");
     //request.setRequestHeader("Content-Length", "222");
     var data = JSON.stringify({
@@ -18,19 +24,21 @@ function EnviaCoordenasQualitas() {
         "nombreOperador": "JORGE PONCE"
     }
     );
-    Bitacora.innerHTML = `${data} <br>`;
+    //request.setRequestHeader("Content-Length", data.length);
+    MensajeLog(`Logitud de los datos: ${data.length}`);
+    MensajeLog(`Datos: ${data}`);
     request.send(data);
     // view request status
-    alert(request.status);
-    Bitacora.innerHTML = Contador + ".- Resultado Post. <br>";
-    Bitacora.innerHTML = request.responseText;
-    Bitacora.innerHTML = "<br>";
+    //alert(request.status);
+    MensajeLog(`Datos: ${request.status}`);
+    MensajeLog ( Contador + ".- Resultado Post.");
+    MensajeLog (request.responseText);
     Contador++;
 }
 
 
 $(document).ready(function () {
-    Bitacora.innerHTML = "1.- Inicio Exitoso <br>";
+    MensajeLog("1.- Inicio Exitoso");
     var CicloEnvio = setInterval(EnviaCoordenasQualitas, 5000);
-    Bitacora.innerHTML = "2.- Ciclo para envío de datos iniciado.";
+    MensajeLog("2.- Ciclo para envío de datos iniciado.");
 });
